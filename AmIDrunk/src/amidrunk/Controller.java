b8edfea9c26b61c753588a49020c266c8c7a18fb
Genís Bosch <genis.bosch@est.fib.upc.edu>
2018-10-19 22:56:19 +0200

    Controller added

---
+++ AmIDrunk/src/amidrunk/Controller.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amidrunk;
+import java.util.*;
+
+/**
+ *
+ * @author genis
+ */
+public class Controller {
+    
+    private ArrayList<Drinker> drinkers;
+            
+    public Controller (){
+        drinkers = new ArrayList<Drinker>();
+
+    }
+}
