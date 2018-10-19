ccd207def3c72bec65bc8be8ebbcc01a19fea16c
Genís Bosch <genis.bosch@est.fib.upc.edu>
2018-10-20 00:42:12 +0200

    cracks yo ya estoy

--- AmIDrunk/src/amidrunk/Controller.java
+++ AmIDrunk/src/amidrunk/Controller.java
@@ -5,6 +5,7 @@
  */
 package amidrunk;
 import java.util.*;
+import java.io.*;
 
 /**
  *
@@ -13,9 +14,34 @@ import java.util.*;
 public class Controller {
     
     private ArrayList<Drinker> drinkers;
+    private File f;
             
     public Controller (){
         drinkers = new ArrayList<Drinker>();
+        
 
+    }
+    public void writeFile(String line){
+    
+        String ruta = "data.txt";
+        File f = new File (ruta);
+        BufferedWriter bw;
+        try{
+            bw = new BufferedWriter(new  FileWriter(f)); 
+            bw.write(line);
+            bw.close();
+        } catch (IOException a) {
+                
+        }
+    }
+    
+    public String showFile(int line) throws FileNotFoundException, IOException{
+    
+        String str;
+        int count = 0;
+        FileReader f = new FileReader("data.txt");
+        BufferedReader b = new BufferedReader(f);
+        while((str = b.readLine())!= null && line > count);
+        return str;
     }
 }
