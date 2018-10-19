6880c7db69be815ba983a43f09ef80d605dc4988
Jesus <kururubest@gmail.com>
2018-10-19 22:40:01 +0200

    clase Drink con atributos y getters de time y grams

--- AmIDrunk/src/amidrunk/Drink.java
+++ AmIDrunk/src/amidrunk/Drink.java
@@ -10,5 +10,27 @@ package amidrunk;
  * @author kurur
  */
 public class Drink {
+    private char name;
+    private int graduation;
+    private int volum;
+    private double grams;
+    private int drinktime;
+
+    public Drink(char name, int graduation, int volum, int drinktime) {
+        this.name = name;
+        this.graduation = graduation;
+        this.volum = volum;
+        this.drinktime = drinktime;
+        this.grams = ((volum*graduation*0.8)/100) ;
+    }
+
+    public double getGrams() {
+        return grams;
+    }
+
+    public int getDrinktime() {
+        return drinktime;
+    }
+    
     
 }
