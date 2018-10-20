647ec9d052d95a63504f3935db2cdbdd622e626c
Jesus <kururubest@gmail.com>
2018-10-20 10:20:48 +0200

    clase time actualizada 1

--- AmIDrunk/src/amidrunk/Time.java
+++ AmIDrunk/src/amidrunk/Time.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package amidrunk;
-
+import java.util.Calendar;
+import java.util.Date;
+     
 /**
  *
  * @author kurur
  */
 public class Time {
+
+
+public double GetActualtime(){
     
+return 0;
+}
+public double GetActualDate(){
+return 0;
+}
+public double GetActualMonth(){
+return 0;    
+}
 }
