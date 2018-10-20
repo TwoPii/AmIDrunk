f52dae4d29d453123b5abed3cc4db790e08855a1
Sergapls <sergi_snv@hotmail.es>
2018-10-20 12:09:14 +0200

    Update Drinker

--- AmIDrunk/src/amidrunk/Drinker.java
+++ AmIDrunk/src/amidrunk/Drinker.java
@@ -17,13 +17,23 @@ public class Drinker {
     private int timeToBeFine;
     private String name;
     private ArrayList<Drink> drinksArray;
+    public static double R;
+    public static double B;
 
     public Drinker(String name, int weight, boolean gender, int numberOfDrinks) {
         this.name = name;
         this.weight = weight;
         this.gender = gender;
         this.drinksArray = new ArrayList<Drink>(numberOfDrinks);
+        if(gender){
+            R = 0.55;
+            B = 0.0025;
+        } else {
+            R = 0.68;
+            B = 0.0026;
+        }
     }
+           
 
     public String getName(){
             return this.name;
@@ -56,7 +66,7 @@ public class Drinker {
         this.gender = gender;
     }
 
-    public void setTimeToBeFine(int timeToBeFine) {
+    public void calculateTimeToBeFine(int timeToBeFine) {
         this.timeToBeFine = timeToBeFine;
     }
 
@@ -64,7 +74,17 @@ public class Drinker {
         this.drinksArray.add(d);       
     }
     
-    public float calculateAlcoholism(){
+    public double actualAlcoholism(){
+        double[] alcoholGrams = new double[this.drinksArray.size()];
+        double[] initialBAC = new double[this.drinksArray.size()];
+        double finalBAC = 0;
+        for(int i = 0; i < this.drinksArray.size(); i++){
+            alcoholGrams[i] = (double) (this.drinksArray.get(i).getVolum()*this.drinksArray.get(i).getGraduation()*0.8);           
+            initialBAC[i] = alcoholGrams[i] * 100 / this.weight / this.R;
+            finalBAC = initialBAC[i] - B * this.drinksArray.get(i).getDrinktime() + finalBAC;
+        }
+        
+        
         
         
         return 0;
