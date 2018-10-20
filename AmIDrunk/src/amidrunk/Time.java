8d00e19922619ac6ca3ab22048c3692f71f9357b
Elsrin <guillemdecabanyes@gmail.com>
2018-10-20 11:33:57 +0200

    Time actualizado

--- AmIDrunk/src/amidrunk/Time.java
+++ AmIDrunk/src/amidrunk/Time.java
@@ -23,4 +23,34 @@ return Calendar.get(Calendar.MINUTE);
 public int GetActualsec(){
 return Calendar.get(Calendar.SECOND);
 }
+public int GetActualmonth(){
+return Calendar.get(Calendar.MONTH);
+}
+public int GetActualday(){
+return Calendar.get(Calendar.DATE);
+}
+public int GetActualyear(){
+return Calendar.get(Calendar.YEAR);
+}
+public double GetCodeDate(){
+int h = this.GetActualhour();
+int m = this.GetActualmin();
+int s = this.GetActualsec();
+int M = this.GetActualmonth();
+int d = this.GetActualday();
+int y = this.GetActualyear();
+return s+m*100+h*10000+d*1000000+M*100000000;
+}
+/*   public static void main(String[] args) {
+    Time t = new Time();
+    int h = t.GetActualhour();
+    int m = t.GetActualmin();
+    int s = t.GetActualsec();
+    int M = t.GetActualmonth();
+    int d = t.GetActualday();
+    int y = t.GetActualyear();
+    
+    System.out.println(h+":"+m+":"+s+" del dia "+d+ " del mes " +M+ " del año" +y);
+    System.out.println(t.GetCodeDate());
+}*/
 }
